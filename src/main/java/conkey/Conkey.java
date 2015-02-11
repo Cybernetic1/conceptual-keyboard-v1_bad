bce5a9f637776ca4ff251ee23f6c6f242390b77e
Cybernetic1 <generic.intelligence@gmail.com>
2015-02-11 20:38:27 +0800

    now using SparkJava version 2.1

---
+++ src/main/java/conkey/Conkey.java
@@ -0,0 +1,218 @@
+package conkey;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
+// import org.apache.log4j.BasicConfigurator;
+
+public class Conkey {
+
+	public static Map<String, Object> staticpages = new HashMap();
+
+	public static String getLocalTextFile(File file) throws IOException {
+		int len;
+		char[] chr = new char[4096];
+		final StringBuffer buffer = new StringBuffer();
+		try (FileReader reader = new FileReader(file)) {
+			while ((len = reader.read(chr)) > 0) {
+				buffer.append(chr, 0, len);
+			}
+		}
+		return buffer.toString();
+	}
+
+	public static void getLocalBinaryFile(File file, OutputStream os) throws IOException {
+
+		byte[] buf = new byte[1024];
+                OutputStreamWriter out;
+            try (FileInputStream in = new FileInputStream(file)) {
+                out = new OutputStreamWriter(os);
+                int count;
+                while ((count = in.read(buf)) >= 0) {
+                    os.write(buf, 0, count);
+		}
+            }
+		out.close();
+
+	}
+
+	public static String getStaticTextFile(String path) throws IOException {
+		if (staticpages.containsKey(path)) {
+			return (String) staticpages.get(path);
+		}
+		String content = getLocalTextFile(new File("./web/" + path));
+		staticpages.put(path, content);
+		return content;
+	}
+
+	public static void getStaticBinaryFile(String path, OutputStream os) throws IOException {
+		getLocalBinaryFile(new File("./web/" + path), os);
+	}
+
+	public static void main(String[] args) {
+
+		// BasicConfigurator.configure();	// configures log4j logger
+
+		Integer portNumber = 9090;			// Spark will run on port 9090
+                setPort(portNumber);
+
+		System.out.println("YKY set port to: " + portNumber.toString() + "\n");
+
+		// This route seems to be old
+		// Route saveDatabase;
+
+		post("/saveDatabase/:fname", (Request request, Response response) -> {
+                    // System.out.println("saving database....");
+                    response.header("Content-type", "text/html");
+                    // String list = rqst.queryParams().toString();
+                    String fname = request.params(":fname");
+                    // System.out.println("params are: " + list);
+                    String data = request.queryParams("data");
+                    // System.out.println("data is: " + data.substring(0, 100));
+                    try {
+                        try ( // Save in web/ directory
+                                PrintWriter out = new PrintWriter("web/" + fname)) {
+                            out.print(data);
+                        }
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(Conkey.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    return "Database saved";
+                });
+
+		// Route sendPidginMessage;
+
+		post("/sendPidginMessage/:name", (Request request, Response response) -> {
+                    // System.out.println("saving database....");
+                    response.header("Content-type", "text/html");
+                    // String list = rqst.queryParams().toString();
+                    String name = request.params(":name");
+                    System.out.println("param is: " + name);
+                    String data = request.queryParams("data");
+                    System.out.println("data is: " + data);
+                    try {
+                        // Send to Pidgin
+                        Process p = Runtime.getRuntime().exec("/home/yky/NetbeansProjects/conceptual-keyboard/pidgin-message.py " + name + " " + data);
+                    } catch (IOException ex) {
+                        Logger.getLogger(Conkey.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    return "Pidgin message sent\n";
+                });
+
+		// Route saveChatLog;
+
+		post("/saveChatLog/:name", (Request request, Response response) -> {
+                    // System.out.println("saving database....");
+                    response.header("Content-type", "text/html");
+                    // String list = rqst.queryParams().toString();
+                    String fname = request.params(":name");
+                    System.out.println("param is: " + fname);
+                    String data = request.queryParams("data");
+                    // System.out.println("data is: " + data);
+                    System.out.println("data obtained.");
+                    
+                    // Save file to local directory
+                    File file = new File("/home/yky/logs/" + fname);
+                    try{
+                        try (Writer output = new BufferedWriter(new FileWriter(file))) {
+                            output.write(data);
+                        }
+                        System.out.println("Chat history written.\n");
+                        
+                    } catch(Exception e){
+                        System.out.println("Cannot create file.\n");
+                    }
+                    return "Chat history saved";
+                });
+
+		get("/getPidginNames/*", (request, response) -> {
+                    response.header("Content-type", "text/html; charset=utf-8");
+                    try {
+                            Process p = Runtime.getRuntime().exec("/home/yky/NetbeansProjects/conceptual-keyboard/pidgin-names.py");
+                    } catch (IOException ex) {
+                            Logger.getLogger(Conkey.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    System.out.println("Executed shell script pidgin-names.py.\n");
+
+                    String pidginNames = "";
+                    try {
+                            pidginNames = new String(Files.readAllBytes(Paths.get("/home/yky/NetbeansProjects/conceptual-keyboard/pidgin-names.txt")));
+                    } catch (IOException ex) {
+                            Logger.getLogger(Conkey.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    System.out.println("Read file pidgin-names.txt.\n");
+                return pidginNames;
+		});
+		
+		
+		get("/*", (request, response) -> {
+                    String url = request.pathInfo();
+                    String page = "index.html";
+                    if (!url.equals("/")) {
+                            String xpage = url.substring(url.indexOf("/") + 1);
+                            if (page.length() > 0) {
+                                    page = xpage;
+                            }
+                    }
+
+                    System.out.println(request.pathInfo());
+                    System.out.println(request.url() + " -> " + page);
+
+                    try {
+                            if        (page.endsWith(".jpg")) {
+                                    response.header("Content-type", "image/jpg");
+                                    response.header("Cache-Control", "no-cache");
+                                    getStaticBinaryFile(page, response.raw().getOutputStream());
+                                    return null;
+                            } else if (page.endsWith(".png")) {
+                                    response.header("Content-type", "image/png");
+                                    getStaticBinaryFile(page, response.raw().getOutputStream());
+                                    return null;
+                            } else if (page.endsWith(".ogg")) {
+                                    response.header("Content-type", "audio/ogg");
+                                    getStaticBinaryFile(page, response.raw().getOutputStream());
+                                    return null;
+                            } else if (page.endsWith(".gif")) {
+                                    response.header("Content-type", "image/gif");
+                                    getStaticBinaryFile(page, response.raw().getOutputStream());
+                                    return null;
+                            } else if (page.endsWith(".ico")) {
+                                    response.header("Content-type", "image/x-icon");
+                                    getStaticBinaryFile(page, response.raw().getOutputStream());
+                                    return null;
+                            } else if (page.endsWith(".html") || page.endsWith(".htm")) {
+                                    response.header("Content-type", "text/html; charset=utf-8");
+                                    return getStaticTextFile(page);
+                            } else {
+                                    return getStaticTextFile(page);
+                            }
+                    } catch (IOException ex) {
+                            try {
+                                    halt(404, getStaticTextFile("404.html"));
+                            } catch (IOException ex1) {
+                                    Logger.getLogger(Conkey.class.getName()).log(Level.SEVERE, null, ex1);
+                            }
+                    }
+                    return null;
+		});
+	
+	}
+}
