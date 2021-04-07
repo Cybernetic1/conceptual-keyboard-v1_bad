7037016677d98f47b28d69197ae86c532a657b4a
Cybernetic1 <generic.intelligence@gmail.com>
2021-04-07 22:24:49 +0800

    added neo4j database dump

--- build-graph-DB.py
+++ build-graph-DB.py
@@ -1,8 +1,31 @@
+# Install Neo4j
+# =============
+# Install Neo4j python driver:
+#	pip3 install neo4j
+# Install Neo4j desktop on Ubuntu, follow instructions from this link:
+#	https://neo4j.com/docs/operations-manual/current/installation/linux/debian
+#		/#debian-installation
+# Set password:
+#	sudo neo4j-admin set-initial-password l0wsecurity --require-password-change
+# Open Neo4j browser:
+#	http://localhost:7474/browser/
+# First time use password: neo4j / neo4j
+# Change password in browser.
+# Now you're ready to run this program.
+
+# Stop database and save a copy
+# =============================
+# command line:
+#	> neo4j stop
+#	> service neo4j stop
+# 	> sudo neo4j-admin dump --to=graph-database.db 
+
 from neo4j import GraphDatabase
 import re
 
 uri = "neo4j://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "l0wsecurity"))
+#driver = GraphDatabase.driver(uri, auth=("neo4j", "neo4j"))
 
 def create_word(tx, chars):
 	tx.run("CREATE (a :Word {chars: $chars})",
