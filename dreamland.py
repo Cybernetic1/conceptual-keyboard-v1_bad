3a825aecd9ae4122f817ebcb11886719edd7b774
Cybernetic1 <generic.intelligence@gmail.com>
2021-08-01 21:38:55 +0800

    dreamland works nicely

--- dreamland.py
+++ dreamland.py
@@ -12,10 +12,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 from sseclient import SSEClient
 import threading
+lock = threading.Lock()
+
 import time
+from playsound import playsound
 
 eventStream = SSEClient('http://localhost:8484/dreamstream',
 	headers={'Content-type': 'text/plain; charset=utf-8'})
@@ -37,7 +41,7 @@ print("Wait for frame exited")
 #driver.get("http://ip131.ek21.com/type_area?roomid=oaca_1&cserial=26MH_TK_CXKd")
 #print("Got sub-URL")
 
-#driver.switch_to_default_content()
+driver.switch_to_default_content()
 #driver.switch_to.frame(driver.find_element_by_css_selector("frame[name='ta']"))
 #driver.switch_to.frame(driver.find_element_by_name("ta"))
 driver.switch_to.frame("ta")
@@ -57,17 +61,22 @@ def consume():
 			callback(s)
 
 def callback(s):
+	global inbox, sendbutt
 	print("fire.data:", s)
-	inbox.send_keys(s)
-	sendbutt.click()
+	with lock:
+		driver.switch_to_default_content()
+		driver.switch_to.frame("ta")
+		inbox.send_keys(s)
+		sendbutt.click()
 
 t = threading.Thread(target=consume)
 t.start()
 
-driver.switch_to.frame("marow")
-print("Switched to frame 'marow'")
-chats = driver.find_element_by_name("main")
-print("Found main chat window:", chats)
+driver.switch_to_default_content()
+driver.switch_to.frame("ma")
+print("Switched to frame 'ma'")
+lastChatLine = driver.find_element_by_xpath("/html/body/div[7]/div[last()]")
+print("Found last chat line:", lastChatLine.text)
 
 # html = document.getElementById("marow").childNodes[3].childNodes[3].contentDocument.childNodes[0];
 # this is the <div> element containing the rows:
@@ -77,9 +86,37 @@ print("Found main chat window:", chats)
 # if ((chatWin != null) && (lastIndex > lastIp131Index)) {
 
 # Loop to record chat log:
+previous = ""
 while True:
-	time.sleep(5)	# in seconds
-	print("waiting...")
+	time.sleep(0.1)	# in seconds
+	with lock:
+		driver.switch_to_default_content()
+		driver.switch_to.frame("ma")
+		lastChatLine = driver.find_element_by_xpath("/html/body/div[7]/div[last()]")
+		# /html/body/div[7]/div[last()]/p/font
+		# /html/body/div[7]/div[9]/p/font/a/font
+		if "進入1k情色皇朝聊天室" in lastChatLine.text:
+			try:
+				fontElement = lastChatLine.find_element_by_xpath("./p/font/a/font")
+				sexColor = fontElement.get_attribute("color")
+			except NoSuchElementException:
+				sexColor = ""
+		else:
+			fontElement = None
+	if lastChatLine.text != previous:
+		# Chat window has new content, log to file:
+		print(lastChatLine.text)
+		# Alert if talk directly at me:
+		if "對 訪客_Cybernetic1" in lastChatLine.text:
+			playsound("dreamland-talk-to-me.wav")
+		# Alert if new comer joins chat:
+		if fontElement:
+			#print("********* New comer joined")
+			#print("fondElement =", fontElement)
+			print("sexColor =", sexColor)
+			if sexColor == "#FF88FF":
+				playsound("dreamland-new-joiner.wav")
+		previous = lastChatLine.text
 
 driver.close()
 exit(0)
