f6a3f5ad2222c0432e9bf444582aae8fa9f28eb8
Cybernetic1 <generic.intelligence@gmail.com>
2021-08-09 08:20:38 +0800

    selenium exception

--- dream.py
+++ dream.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
+from selenium.common.exceptions import WebDriverException, NoSuchElementException, UnexpectedAlertPresentException
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from sseclient import SSEClient
