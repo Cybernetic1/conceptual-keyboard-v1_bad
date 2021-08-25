cc38f50a5d37946188cbd1ef591cf077b2a2e1b2
Cybernetic1 <generic.intelligence@gmail.com>
2021-08-25 09:17:09 +0800

    load firefox script

--- load-firefox-conkey.sh
+++ load-firefox-conkey.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 xclip -selection clipboard /home/yky/misc-programs/conceptual-keyboard/load-conkey.txt
 beep
-# firefox about:debugging#/runtime/this-firefox &
-# echo $win_id
+firefox about:debugging#/runtime/this-firefox
+sleep 1
+# **** xdotool sucks, window name search doesn't work
+# win_id=$(xdotool search --name --onlyvisible "Debugging - Runtime / this-firefox");
+# win_id=$(xdotool search --name --onlyvisible "Firefox");
+win_hex_id=$(wmctrl -l | grep 'Debugging - Runtime / this-firefox' | grep -Eo '0x[0-9a-f]+')
+win_id=$(printf "%d\n" $win_hex_id)
+echo "Win ID =" $win_id
 if [ $(hostname) = 'Vivobook' ]; then
-	firefox about:debugging#/runtime/this-firefox
-	# not sure why but the window name search isn't working
-	sleep 0.7
-	win_id=$(xdotool search --name --onlyvisible "Firefox");
-	# win_id=$(xdotool search --name --onlyvisible "Debugging - Runtime / this-firefox");
 	xdotool windowfocus --sync $win_id mousemove 880 320 click 1
 else
-	firefox about:debugging#/runtime/this-firefox
-	sleep 0.5
-	win_id=$(xdotool search --name --onlyvisible "Mozilla Firefox");
 	xdotool windowfocus --sync $win_id mousemove 228 107 click 3 sleep 1 mousemove 105 265 click 3 sleep 1 mousemove 847 345 click 3
 fi
-sleep 1.0
+sleep 1
 win_id=$(xdotool search --name --onlyvisible "Select manifest.json file")
 # echo $win_id
 xdotool windowfocus --sync $win_id key ctrl+v sleep 0.5 key Return
